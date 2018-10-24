3e93c7e580d67aa983fde31b748eba3b5b45d63e
Henry K <mykim1891@hotmail.com>
2018-10-24 16:20:04 -0700

    Basic User model and controller

---
+++ Users/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 11;
+
+const User = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+  },
+});
+
+// pre-hook for the document -- before saving it
+User.pre('save', function(next) {
+	// once password is encrypted, call next()
+	bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+		if (err) return next(err);
+		this.password = hash;
+		next();
+	});
+});
+
+// Mongoose Method
+User.methods.checkPassword = function(plainPW, cb) {
+	// controller needs to send info here for password comparison
+	// once user is found, compared the encrypted and plaintext PW
+	bcrypt.compare(plainPW, this.password, (err, isMatch) => {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+module.exports = mongoose.model('User', User);
--- server.js
+++ server.js
@@ -2,26 +2,40 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
-// Routes
-const userRouter = require('./Users/userController')
+// Routes (not yet implemented)
+// const userRouter = require('./Users/userController');
 
-const server = express()
+const server = express();
 const corsOption = {
-  origin: 'http://localhost:3000',
-  credentials: true
-}
+	origin: 'http://localhost:3000',
+	credentials: true
+};
 
 // middleware
-server.use(helmet())
-server.use(express.json())]
-server.use(cors(corsOption))
+server.use(helmet());
+server.use(express.json());
+server.use(cors(corsOption));
 
-// mongoose - local
+// mongoose
 mongoose
-.connect("mongodb://localhost/Remindme")
-.then(res => {
-  console.log('-= Connected to mongo DB =-')
-}).catch(err => {
-  console.log("!Error, trouble connecting to mongo DB!")
-})
+	.connect('mongodb://localhost/Remindme') // local
+	// .connect('mongodb://admin:adminpw@ds239940.mlab.com:39940/lambda-note_h') // mlab
+	.then((res) => {
+		console.log('-= Connected to mongo DB =-');
+	})
+	.catch((err) => {
+		console.log('!Error, trouble connecting to mongo DB!');
+	});
+
+// sanity check
+server.get('/', (req, res) => {
+	res.json({ Message: 'API is working!' });
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, (err) => {
+	if (err) console.log(err);
+	console.log(`== Server running on port ${port} ==`);
+});
